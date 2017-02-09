0aafcb4829ee4ce2369ac2dcd3fb95e595ec2cc5
Joseph Viviano <joseph@viviano.ca>
2017-02-09 14:43:37 -0500

    fully tested multiclass, target, and ysplit modes

--- test.sh
+++ test.sh
@@ -1,33 +1,31 @@
 #!/bin/bash
-#echo 'diagnosis prediction, all sites, 50% PCA variance retained'
+#echo 'hc/scz diagnosis prediction using resting state and xbrain correlations (IM/OB), all sites, 50% PCA variance retained'
 #./bin/xbrain \
 #    --xcorr='ts_imi_resid,ts_obs_resid' \
 #    --connectivity='ts_rst' \
 #    --predict='Diagnosis' \
-#    --target-cutoff=0.5 \
 #    --pct-variance=0.5 \
 #    --target-group=0 \
 #    --debug \
 #    /projects/jviviano/data/xbrain/assets/database_xbrain.csv
 
-echo 'multiclass diagnosis prediction using resting state, CMH, 80% PCA variance retained'
+#echo 'multiclass diagnosis prediction using resting state, CMH, 80% PCA variance retained'
+#./bin/xbrain \
+#    --connectivity='ts_rst' \
+#    --predict='Diagnosis' \
+#    --pct-variance=0.80 \
+#    --method='multiclass' \
+#    --debug \
+#    /projects/jviviano/data/xbrain/assets/database_xbrain_CMH.csv
+
+echo 'ysplit social cognitive predicition (bottom 30% vs top 70%) using resting state and xbrain correlations (IM/OB), all sites, 50% PCA variance retained'
 ./bin/xbrain \
     --xcorr='ts_imi_resid,ts_obs_resid' \
     --connectivity='ts_rst' \
-    --predict='Diagnosis' \
-    --pct-variance=0.80 \
-    --target-group=0 \
-    --method='multiclass' \
+    --predict='Part1_TotalCorrect,Part2_TotalCorrect,Part3_TotalCorrect,scog_er40_cr_columnpcr_value' \
+    --pct-variance=0.5 \
+    --y-cutoff=0.3 \
+    --method='ysplit' \
     --debug \
-    /projects/jviviano/data/xbrain/assets/database_xbrain_CMH.csv
+    /projects/jviviano/data/xbrain/assets/database_xbrain.csv
 
-#./bin/xbrain \
-#    --xcorr='ts_imi_resid,ts_obs_resid' \
-#    --connectivity='ts_rst' \
-#    --predict='Part1_TotalCorrect,Part2_TotalCorrect,Part3_TotalCorrect,scog_er40_cr_columnpcr_value' \
-#    --pct-variance=0.8 \
-#    --target-cutoff=0.3 \
-#    --target-group=1 \
-#    --debug \
-#    /projects/jviviano/data/xbrain/assets/database_xbrain.csv
-#
